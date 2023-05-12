4e6311095011eabf02f0359384f106a10b6dabe3
jacf <jacfpentester@gmail.com>
2023-05-13 00:05:37 +0200

    test

---
+++ install.sh
@@ -0,0 +1,10 @@
+
+echo "Tareas (por etapas) del PIPELINE JenkinsFile..."
+
+echo "Instalando la tienda App PC"
+echo "Descargar repositorio de AppTienda"
+echo "Actualizar versiones de los dockerfiles en Docker Hub..."
+echo "Realizar el build del proyecto..."
+echo "Fases de testeo..."
+echo "Matar los contenedores en ejecucion de la version anterior..."
+echo "Levantar los nuevos contenedores actualizados"
